f1869ea28a5287e6cff3540ecc2252c22a5baffe
walid lhaila <walidlhaila00@gmail.com>
2025-02-08 17:47:43 +0100

    Feat: CREATE ENDPOINT TO MAKE USER ADD OLD PROJECTS

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Projects, ProjectsDocument } from './entity/projects.entity';
+import { Model } from 'mongoose';
+import * as jwt from 'jsonwebtoken';
+import { ProjectsDto } from './dto/projects.dto';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(
+    @InjectModel(Projects.name) private ProjectsModel: Model<ProjectsDocument>,
+  ) {}
+
+  async verifyToken(token: string): Promise<string> {
+    const jwtToken = token.split(' ')[1];
+    const decodedToken = jwt.decode(jwtToken) as jwt.JwtPayload;
+    const id = decodedToken?.sub;
+    if (!id) {
+      throw new Error('Invalid token: ID not found.');
+    }
+    return id;
+  }
+
+  async createProjects(projectsDto: ProjectsDto): Promise<ProjectsDocument> {
+    const createProjects = await this.ProjectsModel.create(projectsDto);
+    return createProjects.save();
+  }
+
+}
